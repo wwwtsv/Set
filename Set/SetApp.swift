eb2ddb0ebfba9e37a9191f12c7c80e216ace83ba
aleksejcvetkov <cai.cwet@gmail.com>
2022-05-30 09:56:49 +0300

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Алексей Цветков on 5/30/22.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
